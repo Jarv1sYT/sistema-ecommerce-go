089102cf6ba12858503006e3e07748dd866d94b6
Marlon Rivera <132802162+Jarv1sYT@users.noreply.github.com>
2026-07-19 22:21:44 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("====================================")
+	fmt.Println(" SISTEMA DE GESTIÓN DE E-COMMERCE")
+	fmt.Println("====================================")
+	fmt.Println("Proyecto iniciado correctamente.")
+}
